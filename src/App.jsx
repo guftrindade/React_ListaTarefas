c3d28c966b76015cee10cb5518ac695072e5a4b2
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-05-25 18:27:30 -0300

    chore: implementado delete task

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Header from './components/Header';
 import Tasks from './components/Tasks';
-import './App.css';
 import AddTask from './components/AddTask';
+
+import './App.css';
 
 const App = () => {
 
@@ -43,13 +47,31 @@ const App = () => {
     setTasks(newTasks);
   }
 
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id != taskId);
+    setTasks(newTasks);
+  }
+
   return (
-    <>
+    <Router>
         <div className="container">
-          <AddTask handleTaskAddition={handleTaskAddition}/>
-          <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
+          <Header/>
+          <Route
+            path="/"
+            exact
+            render = {() => (
+              <>
+                <AddTask handleTaskAddition={handleTaskAddition}/>
+                <Tasks 
+                  tasks={tasks} 
+                  handleTaskClick={handleTaskClick}
+                  handleTaskDeletion={handleTaskDeletion}
+                />
+              </>
+            )}
+          />
         </div>
-    </>
+    </Router>
   )
 };
 
