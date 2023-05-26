e71ca24c42e7e45847ef199030ec7adb72df6c4d
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-05-25 21:34:43 -0300

    chore: projeto finalizado

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,11 +6,12 @@ const Tasks = ({tasks, handleTaskClick, handleTaskDeletion}) => {
         <>
             {tasks.map((task) => (
                 <Task 
+                    key={task.id}
                     task = {task} 
                     handleTaskClick={handleTaskClick}
                     handleTaskDeletion={handleTaskDeletion}
                 />
-            ))};
+            ))}
         </>
     )
 }
